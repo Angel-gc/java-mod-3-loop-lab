ce05fc109d76718c92d3bda09986ff140017fe35
Angel Concepcion <angelconcepcion50@gmail.com>
2022-06-22 13:59:49 -0400

    feature: Working Solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,29 @@ public class Main {
         int startingYear = 2000;
         int targetYear = 2011;
         int currentYear = startingYear;
-        while (currentYear < targetYear) {
-            System.out.println((currentYear - startingYear) + " year(s) have passed");
-            // conditional logic based on the current year
-            currentYear++;
+        String sentence = " years have passed";
+
+
+        // while (currentYear < targetYear) {
+        //     //System.out.println((currentYear - startingYear) + " year(s) have passed");
+        //     // conditional logic based on the current year
+        //     if((currentYear - startingYear) == 1){
+        //         System.out.println((currentYear - startingYear) +  " year has passed");
+        //     }
+        //     else if ((currentYear - startingYear) > 1){
+        //         System.out.println((currentYear - startingYear) + sentence);
+        //     }
+        //     currentYear++;
+        // }
+
+        for (int i = 0; currentYear < targetYear; i++){
+            if((currentYear - startingYear) == 1){
+                System.out.println((currentYear - startingYear) +  " year has passed");
+            }
+            else if ((currentYear - startingYear) > 1){
+                System.out.println((currentYear - startingYear) + sentence);
+            }
+            currentYear += 1;
         }
     }
 }
